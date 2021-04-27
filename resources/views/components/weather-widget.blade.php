b7f1e924aaf00932ed61b1663abb048f021b2a32
Stuart Summers <summers2483@hotmail.com>
2021-04-27 11:26:47 +0200

    Blade component and openWeatherAPI test

---
+++ resources/views/components/weather-widget.blade.php
@@ -0,0 +1,87 @@
+<div>
+    <div class="w-128 mx-auto bg-gray-900 text-white text-sm rounded overflow-hidden">
+
+        {{--                Current Weather                 --}}
+        <div class="current-weather relative">
+            <div class="flex items-center justify-between px-4 py-6">
+                <div class="flex-col items-center">
+                    <div class="text-5xl font-semibold">4&#176;C</div>
+                    <div class="text-gray-400">Feels like 5&#176;C</div>
+                </div>
+                {{--                    Description                 --}}
+                <div class="ml-5">
+                    <div class="font-semi-bold">Overcast</div>
+                    <div class="text-gray-400">Berlin, Germany</div>
+                </div>
+
+                <div class="">ICON</div>
+            </div>
+            <button class="absolute right-0 bottom-0 mb-2 mr-2 text-xs">Toggle</button>
+
+        </div>
+        {{--                Future Weather                  --}}
+        <ul class="future-weather bg-gray-700 px-4 py-7 space-y-8">
+            <li class="grid grid-cols-weather items-center">
+                <div class="text-gray-400">Tue</div>
+                <div class="flex items-center">
+                    <div class="">icon</div>
+                    <div class="">Clear Sky</div>
+                </div>
+                <div class="text-right text-xs">
+                    <div>5&#176;C</div>
+                    <div>4&#176;C</div>
+                </div>
+            </li>
+
+            <li class="grid grid-cols-weather items-center">
+                <div class="text-gray-400">Tue</div>
+                <div class="flex items-center">
+                    <div class="">icon</div>
+                    <div class="">Clear Sky</div>
+                </div>
+                <div class="text-right text-xs">
+                    <div>5&#176;C</div>
+                    <div>4&#176;C</div>
+                </div>
+            </li>
+
+            <li class="grid grid-cols-weather items-center">
+                <div class="text-gray-400">Tue</div>
+                <div class="flex items-center">
+                    <div class="">icon</div>
+                    <div class="">Clear Sky</div>
+                </div>
+                <div class="text-right text-xs">
+                    <div>5&#176;C</div>
+                    <div>4&#176;C</div>
+                </div>
+            </li>
+
+            <li class="grid grid-cols-weather items-center">
+                <div class="text-gray-400">Tue</div>
+                <div class="flex items-center">
+                    <div class="">icon</div>
+                    <div class="">Clear Sky</div>
+                </div>
+                <div class="text-right text-xs">
+                    <div>5&#176;C</div>
+                    <div>4&#176;C</div>
+                </div>
+            </li>
+
+            <li class="grid grid-cols-weather items-center">
+                <div class="text-gray-400">Tue</div>
+                <div class="flex items-center">
+                    <div class="">icon</div>
+                    <div class="">Clear Sky</div>
+                </div>
+                <div class="text-right text-xs">
+                    <div>5&#176;C</div>
+                    <div>4&#176;C</div>
+                </div>
+            </li>
+
+        </ul>
+
+    </div>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+//    Geocoding API
+    $location = 'Berlin';
+    $geoAPI = '44163ddba63942ccb24ab41f22b4a9f3';
+    $bounds = '-11.60156,36.31513,22.14844,58.99531';
+
+    $response = Http::get("https://api.opencagedata.com/geocode/v1/json?q={$location}&key={$geoAPI}");
+
+    dd($response->json());
+//    Weather Api
+
+    $lat = '';
+    $lon = '';
+    $apiKey = '8c444def6f6d95a5ebf766bdd5b5ad49';
+
+    $response = Http::get("https://api.openweathermap.org/data/2.5/onecall?lat={$lat}&lon={$lon}&appid={$apiKey}");
+
+    return view('welcome');
+});
